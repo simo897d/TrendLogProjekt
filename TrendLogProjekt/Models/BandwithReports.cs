2f2f309a65c9e4365c62260d3c6b859cbd73c282
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-10-21 13:49:05 +0200

    Add project files.

---
+++ TrendLogProjekt/Models/BandwithReports.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrendLogProjekt.Models
+{
+    public class BandwithReports
+    {
+        double ServerAllocation { get; set;}
+        double GeneratedLeads { get; set; }
+        double SubmittedTickets { get; set; }
+        double GeneratedLeads2 { get; set; }
+    }
+}
